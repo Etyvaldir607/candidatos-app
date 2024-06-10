0f279d06c313c11365620fa935f2df90ccbb23e3
Erick Machicado <etyvaldirmc@gmail.com>
2024-06-10 00:26:46 -0400

    modified - applicant seeder

--- database/seeders/ApplicantSeeder.php
+++ database/seeders/ApplicantSeeder.php
@@ -31,10 +31,6 @@ class ApplicantSeeder extends Seeder
 
         Applicant::factory($this->amount)->make()->each(function ($applicant) use ($progressBar) {
             // advances the progress bar 1 unit
-            if ($this->amount > 5) {
-                $applicant->owner = null;
-            }
-
             if ($applicant->save()) {
                 $progressBar->advance();
             }
